186dd6492bbc9aa398061b3fbc9035ed0ce3af26
teja <bharantheja@users.noreply.github.com>
2026-03-30 11:13:57 +0530

    final merge is resolved

--- git-max1/calcia.py
+++ git-max1/calcia.py
@@ -11,3 +11,27 @@ for x in fruits:
   if x == "melon":
     continue
   print(x)    
+queue = []
+
+# Enqueue
+queue.append('A')
+queue.append('B')
+queue.append('C')
+print("Queue: ", queue)
+
+# Peek
+frontElement = queue[0]
+print("Peek: ", frontElement)
+
+# Dequeue
+poppedElement = queue.pop(0)
+print("Dequeue: ", poppedElement)
+
+print("Queue after Dequeue: ", queue)
+
+# isEmpty
+isEmpty = not bool(queue)
+print("isEmpty: ", isEmpty)
+
+# Size
+print("Size: ", len(queue))
